be0efec895bd233cf0bf9eafb9c78e716142e9df
Brian Nguyen <briannguyen@Brians-MacBook-Pro.local>
2024-04-07 17:43:29 -0400

    create basic navbar

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Notice the 'Routes' import
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Transactions from "./pages/Transactions"; // Import Transactions component
+import Bills from "./pages/Bills"; // Import Bills component
+import Settings from "./pages/Settings"; // Import Settings component
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -12,12 +16,25 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <header className="App-header">
+                <p>{!data ? "Loading..." : data}</p>
+              </header>
+            }
+            exact
+          />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/bills" element={<Bills />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

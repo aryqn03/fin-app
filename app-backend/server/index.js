fd11a38b9f4c0b8e71f5c90ac14a63eecccdc343
Aryan <aryanjhaveri3@gmail.com>
2024-06-24 22:28:18 -0400

    transactions call working

--- app-backend/server/index.js
+++ app-backend/server/index.js
@@ -6,10 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const cors = require('cors');
 
-// You can configure CORS globally
-app.use(cors());
-
-// Or configure CORS with specific options
+// Configure CORS
 app.use(cors({
     origin: 'http://localhost:3000',  // Allow only this origin to access
     methods: ['GET', 'POST'],  // Allowed methods
@@ -28,7 +25,6 @@ app.get("/api/accounts", async (req, res) => {
       const accessToken = req.headers.authorization.split(' ')[1]; // Assuming the access token is sent as a Bearer token
       const auth = 'Basic ' + Buffer.from(accessToken + ':').toString('base64'); // Encode as Basic Auth
       const response = await axios.get('https://api.teller.io/accounts', {
-          mode: 'no-cors',
           headers: {
               'Authorization': auth // Pass the encoded access token
           },
@@ -38,6 +34,27 @@ app.get("/api/accounts", async (req, res) => {
   } catch (error) {
       console.error('Failed to fetch accounts:', error);
       res.status(500).send('Failed to fetch accounts');
+  }
+});
+
+// Endpoint to fetch transactions for a specific account
+app.get("/api/accounts/:accountId/transactions", async (req, res) => {
+  try {
+      console.log("Received request for transactions");
+      const accountId = req.params.accountId;  // Get the accountId from the URL parameters
+      console.log("Account ID:", accountId);  // Log the accountId
+      const accessToken = req.headers.authorization.split(' ')[1]; // Assuming the access token is sent as a Bearer token
+      const auth = 'Basic ' + Buffer.from(accessToken + ':').toString('base64'); // Encode as Basic Auth
+      const response = await axios.get(`https://api.teller.io/accounts/${accountId}/transactions`, {
+          headers: {
+              'Authorization': auth // Pass the encoded access token
+          },
+          httpsAgent: httpsAgent // Use the mTLS configured agent
+      });
+      res.json(response.data);
+  } catch (error) {
+      console.error('Failed to fetch transactions:', error);
+      res.status(500).send('Failed to fetch transactions');
   }
 });
 
